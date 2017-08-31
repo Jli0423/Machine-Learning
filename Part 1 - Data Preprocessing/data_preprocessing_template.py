621cca5171ac9835f457468575edd509813cd5f0
jli0423 <justin.li0423@gmail.com>
2017-08-31 00:47:25 -0400

    Data preprocessing

---
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing dataset
+dataset = pd.read_csv('Data.csv')
+#Takes all values except last row
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+
+#Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+#Fitting the missing values into dataset X
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#Encoding Categorial data
+#-----Countries-----
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#label countries by either 0,1,2 (has order)
+labelencoder_X = LabelEncoder()
+x[:, 0] = labelencoder_X.fit_transform(x[:, 0])
+#using oneHotEncoder by bits for unordered list
+onehotencoder = OneHotEncoder(categorical_features = [0])
+x = onehotencoder.fit_transform(x).toarray()
+
+#-----Dependent Variable(yes/no)-----
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
