ecf140040be241bdb6ece8ac7993b84f078acbaf
jli0423 <justin.li0423@gmail.com>
2017-08-31 22:51:46 -0400

    Feature Scaling

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -33,3 +33,15 @@ y = labelencoder_y.fit_transform(y)
 #Splitting dataset into training and test
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+#Feature scaling: Makes sure euclidean distance isn't dominated by 1 set of values
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+
+
+
+
+
