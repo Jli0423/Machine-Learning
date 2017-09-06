7c44adc77cccdc0be789db4eecf694fa3d1b6986
jli0423 <justin.li0423@gmail.com>
2017-09-06 17:14:37 -0400

    Finished multi linear regression model

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
@@ -10,6 +10,18 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
+#OneHotEncoder to remove categorial data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#label countries by either 0,1,2 (has order)
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+#using oneHotEncoder by bits for unordered list
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the dummy variable trap(Py does this automatically)
+X = X[:, 1:]
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -21,3 +33,11 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+#Fitting multi linear regression to training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting test set result
+y_pred = regressor.predict(X_test)
